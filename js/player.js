914580ffad935638e6bb7889e76174422d6e01e2
BrandiBQ <35387365+BrandiBQ@users.noreply.github.com>
2018-04-13 13:28:22 -0700

    Add files via upload

--- js/player.js
+++ js/player.js
@@ -6,8 +6,15 @@
     }
 }*/
 
+var eBalance = $("#balance").html();
+var balance = parseInt(eBalance); 
+var eTurnCount = $("#turnCount").html();    
+var turnCount = parseInt(eTurnCount); 
+
 var player = function (pName){
     this.name = pName;
+    this.playerBalance = balance;
+    this.playerTurn = turnCount;
     this.getName = function(){
         return this.name;
     }
